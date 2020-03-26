af7bdcdbcd00f5fb2527f411628a87064286a9f7
zhuo <lwzhuo@live.com>
2020-03-26 14:16:50 +0800

    支持websocket

---
+++ imsystem/src/main/java/com/zhuo/imsystem/websocket/handler/TextWebSocketFrameHandler.java
@@ -0,0 +1,23 @@
+package com.zhuo.imsystem.websocket.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.time.LocalDateTime;
+
+//处理文本协议数据，处理TextWebSocketFrame类型的数据，websocket专门处理文本的frame就是TextWebSocketFrame
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    //读到客户端的内容并且向客户端去写内容
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println("收到消息："+msg.text());
+
+        /**
+         * writeAndFlush接收的参数类型是Object类型，但是一般我们都是要传入管道中传输数据的类型，比如我们当前的demo
+         * 传输的就是TextWebSocketFrame类型的数据
+         */
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("服务时间："+ LocalDateTime.now()));
+    }
+}
--- imsystem/src/main/java/com/zhuo/imsystem/websocket/handler/WebSocketChildChannelHandler.java
+++ imsystem/src/main/java/com/zhuo/imsystem/websocket/handler/WebSocketChildChannelHandler.java
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 import org.springframework.stereotype.Component;
 
@@ -14,5 +15,11 @@ public class WebSocketChildChannelHandler extends ChannelInitializer<SocketChann
         ch.pipeline().addLast("http-codec", new HttpServerCodec()); // HTTP编码解码器
         ch.pipeline().addLast("aggregator", new HttpObjectAggregator(65536)); // 把HTTP头、HTTP体拼成完整的HTTP请求
         ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler()); // 方便大文件传输，不过实质上都是短的文本数据
+        //ws://server:port/context_path
+        //ws://localhost:9999/ws
+        //参数指的是contex_path
+        ch.pipeline().addLast(new WebSocketServerProtocolHandler("/ws"));
+        //websocket定义了传递数据的6中frame类型
+        ch.pipeline().addLast(new TextWebSocketFrameHandler());
     }
 }

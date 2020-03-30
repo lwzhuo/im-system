daa4009f6984f2e41c22ce0771ba6fcb43c4dcab
zhuo <lwzhuo@live.com>
2020-03-30 15:08:48 +0800

    解决消息无法响应的问题

--- imsystem/src/main/java/com/zhuo/imsystem/websocket/handler/TextWebSocketFrameEchoHandler.java
+++ imsystem/src/main/java/com/zhuo/imsystem/websocket/handler/TextWebSocketFrameEchoHandler.java
@@ -3,6 +3,7 @@ package com.zhuo.imsystem.websocket.handler;
 import com.zhuo.imsystem.websocket.protocal.EchoProtocal;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 
 public class TextWebSocketFrameEchoHandler extends SimpleChannelInboundHandler<EchoProtocal> {
     //读到客户端的内容并且向客户端去写内容
@@ -10,6 +11,6 @@ public class TextWebSocketFrameEchoHandler extends SimpleChannelInboundHandler<E
     protected void channelRead0(ChannelHandlerContext ctx, EchoProtocal msg) throws Exception {
         String recMsg = msg.getMsg();
         System.out.println("收到echo:"+recMsg);
-        ctx.writeAndFlush("echo"+recMsg);
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("echo:"+recMsg));
     }
 }
--- imsystem/src/main/java/com/zhuo/imsystem/websocket/handler/TextWebSocketFrameSingleChatHandler.java
+++ imsystem/src/main/java/com/zhuo/imsystem/websocket/handler/TextWebSocketFrameSingleChatHandler.java
@@ -3,12 +3,13 @@ package com.zhuo.imsystem.websocket.handler;
 import com.zhuo.imsystem.websocket.protocal.SingleChatProtocal;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 
 public class TextWebSocketFrameSingleChatHandler extends SimpleChannelInboundHandler<SingleChatProtocal> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, SingleChatProtocal msg) throws Exception {
         String recMsg = msg.getMsg();
         System.out.println("收到单聊请求:"+recMsg);
-        ctx.writeAndFlush("singlechat"+recMsg);
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("singlechat:"+recMsg));
     }
 }

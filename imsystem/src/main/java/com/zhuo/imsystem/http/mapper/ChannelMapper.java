2334e4b7a911b1243a48509f0563bc74510b99bf
zhuo <lwzhuo@live.com>
2020-05-07 14:47:08 +0800

    支持summary显示

--- imsystem/src/main/java/com/zhuo/imsystem/http/mapper/ChannelMapper.java
+++ imsystem/src/main/java/com/zhuo/imsystem/http/mapper/ChannelMapper.java
@@ -11,35 +11,38 @@ public interface ChannelMapper {
     @InsertProvider(type = ChannelSQLBulider.class, method = "insertChannel")
     public int saveChannel(ChannelDto channelDto);
 
-    @Results(id = "privateChannelResult1", value = {
+    @Results(id = "ChannelResult1", value = {
             @Result(property = "channelId", column = "channel_id", id = true),
             @Result(property = "channelName", column = "channel_name"),
             @Result(property = "creatorId", column = "creator_id"),
             @Result(property = "attenderId", column = "attender_id"),
             @Result(property = "channelType", column = "channel_type"),
             @Result(property = "picUrl", column = "pic_url"),
+            @Result(property = "summary", column = "summary"),
     })
     @Select("select * from im_channel where creator_id=#{creatorId} and attender_id=#{attenderId} limit 1")
     public ChannelDto queryPrivateChannelByMemberUid(String creatorId,String attenderId);
 
-    @Results(id = "privateChannelResult2", value = {
+    @Results(id = "ChannelResult2", value = {
             @Result(property = "channelId", column = "channel_id", id = true),
             @Result(property = "channelName", column = "channel_name"),
             @Result(property = "creatorId", column = "creator_id"),
             @Result(property = "attenderId", column = "attender_id"),
             @Result(property = "channelType", column = "channel_type"),
             @Result(property = "picUrl", column = "pic_url"),
+            @Result(property = "summary", column = "summary"),
     })
     @Select("select * from im_channel where channel_id=#{channelId}")
     public List<ChannelDto> queryChannelInfoByChannelId(String ChannelId);
 
-    @Results(id = "privateChannelResult3", value = {
+    @Results(id = "ChannelResult3", value = {
             @Result(property = "channelId", column = "channel_id", id = true),
             @Result(property = "channelName", column = "channel_name"),
             @Result(property = "creatorId", column = "creator_id"),
             @Result(property = "attenderId", column = "attender_id"),
             @Result(property = "channelType", column = "channel_type"),
             @Result(property = "picUrl", column = "pic_url"),
+            @Result(property = "summary", column = "summary"),
     })
     @Select("select * from im_channel where creator_id=#{creatorId} and channel_id=#{channelId} limit 1")
     public ChannelDto queryPrivateChannelByCreatorUid(String channelId,String creatorId);
